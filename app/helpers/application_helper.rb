6ebdcf1642dad9738533bdf441d0c3e0d591d571
ijones16 <jones58ian@gmail.com>
2020-05-20 19:12:49 -0400

    Style registrations/new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+   def flash_messages(_opts = {})
+    # return if flash[0][0] == :form_error
+    flash.each do |msg_type, message|
+      next if msg_type == :form_error
+      concat(content_tag(:div, message.html_safe, {
+          class: "alert fade show"}) do
+          concat content_tag(:button, 'x', class: "close", data: {
+              dismiss: 'alert'
+          })
+        concat message.html_safe
+      end)
+    end
+    nil
+  end
 end
---
+++ app/javascript/lib/flash.js
@@ -0,0 +1,5 @@
+$(document).on('turbolinks:load', () =>
+  $('.alert')
+    .delay(5000)
+    .slideUp(500, () => $('.alert').alert('close')),
+)

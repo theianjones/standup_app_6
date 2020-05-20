3d7a3c58dff0a406164e460ff3dab81156e5701e
ijones16 <jones58ian@gmail.com>
2020-05-20 17:22:39 -0400

    get activity controller set up

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,15 @@
-const { environment } = require('@rails/webpacker')
+const {environment} = require('@rails/webpacker')
+
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    jquery: 'jquery',
+    'window.Tether': 'tether',
+  }),
+)
 
 module.exports = environment
